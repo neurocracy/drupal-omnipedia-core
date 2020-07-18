fa233ac4257449bb5f92495eb0c8bf80b7a79886
Matei Stanca <i@ambientimpact.com>
2020-07-17 20:50:52 -0400

    WikiNodeTrackerInterface: documentation formatting pass.

--- src/Service/WikiNodeTrackerInterface.php
+++ src/Service/WikiNodeTrackerInterface.php
@@ -15,19 +15,19 @@ interface WikiNodeTrackerInterface {
    * @return array
    *   An array with saved wiki node data, with the following top level keys:
    *
-   *   - nodes: an array with keys corresponding to node IDs, with each value
+   *   - 'nodes': An array with keys corresponding to node IDs, with each value
    *     containing an array with the following:
    *
-   *     - 'date': the node's date as a string in 'storage' format
+   *     - 'date': The node's date as a string in 'storage' format.
    *
-   *     - 'title': string title of the node
+   *     - 'title': String title of the node.
    *
-   *     - 'published': boolean indicating whether this node is published
+   *     - 'published': Boolean indicating whether this node is published
    *
-   *   - dates: an array with dates in the 'storage' format as the keys, each
-   *     containing an array of node IDs
+   *   - 'dates': An array with dates in the 'storage' format as the keys, each
+   *     containing an array of node IDs.
    *
-   *   - titles: an array whose keys are node IDs and values are their titles
+   *   - 'titles': An array whose keys are node IDs and values are their titles.
    *
    * @see \Drupal\omnipedia_core\Service\WikiNodeTrackerInterface::trackWikiNode()
    *   Starts tracking or updates tracking of a wiki node.
