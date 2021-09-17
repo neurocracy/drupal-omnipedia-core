4e1d959af684f73907ef15c2c106135bd0f40818
Matei Stanca <i@ambientimpact.com>
2021-09-16 21:11:09 -0400

    WikiNodeRoute now considers 'entity.node.edit_form' as viewing a node:
    This is to fix an issue with the <main-page> element incorrectly setting
    an error if your current date based on the last node viewed was
    different from the node being edited, which could happen if you went
    straight to the node edit form from another admin page, e.g. the content
    overview.

--- src/Service/WikiNodeRoute.php
+++ src/Service/WikiNodeRoute.php
@@ -12,11 +12,17 @@ class WikiNodeRoute implements WikiNodeRouteInterface {
   /**
    * Route names that are considered as viewing a wiki node.
    *
+   * Note that some of these need to be here to be considered when setting the
+   * current date, as the current date needs to be updated even when not viewing
+   * 'entity.node.canonical', for example, but also 'entity.node.edit_form' to
+   * ensure any input filters that validate based on the date do so correctly.
+   *
    * @var array
    */
   protected $wikiNodeViewRouteNames = [
     'entity.node.canonical',
     'entity.node.preview',
+    'entity.node.edit_form',
     'entity.node.omnipedia_changes',
   ];
 
