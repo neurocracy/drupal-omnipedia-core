6d500d9eda011926a787b3c9430bb39272d88615
Matei Stanca <i@ambientimpact.com>
2022-06-08 00:06:21 -0400

    Entity\Node: Removed unused core NodeInterface use.

--- src/Entity/Node.php
+++ src/Entity/Node.php
@@ -3,7 +3,6 @@
 namespace Drupal\omnipedia_core\Entity;
 
 use Drupal\node\Entity\Node as CoreNode;
-use Drupal\node\NodeInterface as CoreNodeInterface;
 use Drupal\omnipedia_core\Entity\NodeInterface;
 use Drupal\omnipedia_core\Service\WikiNodeMainPageInterface;
 use Drupal\omnipedia_core\Service\WikiNodeRevisionInterface;
